1a548c6a2d0c862f302eb5b05273ffd5fc372bae
Norma M Korosec <norminchqie@gmail.com>
2017-01-17 16:04:17 +0100

    LONDON_Hoteli works! Finally! Just analysis to go.

--- potovanje.py
+++ potovanje.py
@@ -8,22 +8,17 @@ import unicodedata
 
 mesta = ["LONDON", "PARIS", "MOSCOW", "BERLIN", "PRAGUE", "AMSTERDAM", "BERN"]
 
-def potovanje(mesto="LONDON",datum_zacetka_obdobja = date(2017,2,1), datum_konca_obdobja = date(2017,2,10), stevilo_zadetkov=5):
+def potovanje(mesto="LONDON",datum_zacetka_obdobja = date(2017,2,1), datum_konca_obdobja = date(2017,12,31), stevilo_zadetkov=200):
     """Ta funkcija naredi datoteko, v kateri je shranjenih prvih nekaj (toliko kot je
        stevilo zadetkov) najugodnejsih ponudb za vikend nocitev za 2 odrasli osebi v
-       izbranem kraju. Datoteka je sestavljena tako, da so vsi podatki za posamezen
-       datum v eni vrstici
-       datum1, cena11, cena12, ...
-       datum2, cena21, cena22, ..."""
+       izbranem kraju. Datoteka je sestavljena tako, da so vsi podatki za posamezen"""
     
-    ime_datoteke = mesto + "2.csv"
+    ime_datoteke = mesto + ".csv"
     if os.path.isfile(ime_datoteke):
         print("Podatki za to mesto so že shranjeni!")
     else:
         datumi_vikendov = datumi_vikendov_get(fromdate=datum_zacetka_obdobja, todate=datum_konca_obdobja)
-        print(datumi_vikendov)
         vikendi = vikendi_get(datum_zacetka_obdobja, datum_konca_obdobja)
-        print(vikendi)
         csv = ""
         stevec = 0
         hoteli = "hotelId,name,proximityDistance,guestRating,freeCancellation,shortDescription" + "\n"
@@ -47,30 +42,23 @@ def potovanje(mesto="LONDON",datum_zacetka_obdobja = date(2017,2,1), datum_konca
                     continue
                 else:
                     hoteli += id + ","
-                    hoteli += slovar["hotelList"][j]["name"] + ","
+                    c = slovar["hotelList"][j]["name"]
+                    hoteli += " ".join(c.split(",")) + ","
                     a = slovar["hotelList"][j]["proximityDistanceInKiloMeters"]
                     hoteli += "%.2f" % round(float(a),2) + ","
                     hoteli += slovar["hotelList"][j]["hotelGuestRating"] + ","
                     hoteli += str(slovar["hotelList"][j]["hasFreeCancellation"]) + ","
                     b = slovar["hotelList"][j]["shortDescription"].strip()
                     hoteli += ";".join(b.split(","))  
-                    hoteli += "\n"
-                
-            
+                    hoteli += "\n"    
             stevec += 1
 
-        with open(mesto + "1Hoteli.csv","w") as f:
-            hoteli.encode('ascii', 'ignore')
-            #hoteli = unicode(hoteli, errors= 'ignore')
+        with open(mesto + "_Hoteli.csv","w", encoding = 'utf-8') as f:
             f.write(hoteli)
 
-
         prva_vrstica = "friday,hotelId,price"
-        #for i in range (1, stevilo_zadetkov+1):
-        #    prva_vrstica += ",cena" + str(i)
-
             
-        with open(mesto + "1.csv","w") as f:
+        with open(mesto + ".csv","w") as f:
             csv = prva_vrstica + "\n" + csv
             f.write(csv)
     print("Zaključeno!")
